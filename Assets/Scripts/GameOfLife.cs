ba3195e9037c2c45ae33daff163d8e195ce49ee8
barshp <>
2020-08-30 19:58:49 +0300

    Added cells and grid creation

---
+++ Assets/Scripts/GameOfLife.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOfLife : MonoBehaviour
+{
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private Vector2Int gridSize;
+    [Range(0.00001f, 3f)] private float stepsRate;
+
+    private Vector2 cellSize;
+    private CellController[,] cells;
+
+    void Start()
+    {
+        Camera cam = Camera.main;
+        cam.transform.position = new Vector3(gridSize.x / 2, gridSize.y / 2, cam.transform.position.z);
+
+        var cellSpriteRenderer = cellPrefab.GetComponent<SpriteRenderer>();
+        cellSize = new Vector2(cellSpriteRenderer.sprite.bounds.size.x, cellSpriteRenderer.sprite.bounds.size.y);
+
+        CreateGrid(gridSize.x, gridSize.y);
+    }
+
+    void Update()
+    {
+        // StartCourotine
+        // // 
+    }
+
+    private void CreateGrid(int width, int height)
+    {
+        cells = new CellController[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var cellPos = new Vector2(x * cellSize.x, y * cellSize.y);
+                GameObject cell = Instantiate(cellPrefab, cellPos, Quaternion.identity);
+                cells[x, y] = cell.GetComponent<CellController>();
+            }
+        }
+    }
+}
